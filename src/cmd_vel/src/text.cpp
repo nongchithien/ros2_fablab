5dbcdb76539cc3920d7fd2658003b7e60f25bd4f
nongchithien <thienchinong@gmail.com>
2025-09-10 17:50:28 +0700

    Add new file

---
+++ src/cmd_vel/src/text.cpp
@@ -0,0 +1,11 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <cmath>
+#include "can_node.h"
+#include <cstdlib>
+#include <string>
+#include <map>
+#include <vector>
+#include <chrono> // For time measurement
+#include <ctime>
+#include <iomanip>
